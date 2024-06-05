e4bb72baf4269d934157591bc55ed110a96291de
Andrew <progressmachineuseful18@gmail.com>
2024-06-05 18:18:05 +0300

    Filter Redux slice and filter by title

--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,10 +1,33 @@
+import {useDispatch, useSelector} from "react-redux";
+import {setTitleFilter, selectTitleFilter, resetFilters} from "../../redux/slices/filterSlice";
 import './Filter.css'
 
 const Filter = () => {
+    const dispatch = useDispatch();
+    const titleFilter = useSelector(selectTitleFilter);
+
+    const handleTitleFilterChange = (e) => {
+        dispatch(setTitleFilter(e.target.value));
+    }
+
+    const handleResetFilter = () => {
+        dispatch(resetFilters());
+    }
+
     return (
         <div className="app-block filter">
-            <h2>Filters</h2>
+            <div className="filter-row">
+                <div className="filter-group">
+                    <input type="text"
+                           placeholder="Filter by title..."
+                           onChange={handleTitleFilterChange}
+                           value={titleFilter}
+                    />
+                </div>
+                <button type="button" onClick={handleResetFilter}>Reset Filters</button>
+            </div>
         </div>
+
     )
 }
 
