76286de8de41d20c9309946a073f89a80bc08e22
Andrew <progressmachineuseful18@gmail.com>
2024-06-08 14:45:31 +0300

    Error component with toast notifications

---
+++ frontend/src/components/Error/Error.js
@@ -0,0 +1,23 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {useDispatch, useSelector} from "react-redux";
+import {clearError, selectErrorMessage, setError} from "../../redux/slices/errorSlice";
+import {useEffect} from "react";
+
+const Error = () => {
+    const errorMessage = useSelector(selectErrorMessage)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (errorMessage) {
+            toast.info(errorMessage);
+            dispatch(clearError());
+        }
+    }, [errorMessage, dispatch]);
+
+    return <ToastContainer position="top-right" autoClose={2000}/>;
+
+
+}
+
+export default Error;
